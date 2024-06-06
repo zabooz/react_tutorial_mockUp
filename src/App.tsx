87b4a7786142d7a4ee1b212b71f45acd059136b1
Daniel Osojnik <zabooz1988@gmai.com>
2024-06-06 10:15:38 +0200

    static version

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const products:Product[]= [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
 
+interface Product {
+  category: string
+  price: string
+  stocked : boolean
+  name:string
+}
+
+function ProductCategoryRow({category}:Product){
+  return(
+    <tr>
+      <th colSpan={2}>
+        {category}
+      </th>
+    </tr>
+  )
+}
+function ProductRow({product}:{product:Product}){
+  const name = product.stocked ? product.name:
+    <span style={{color:'red'}}>
+      {product.name}
+    </span>
+
+
+    return(
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    )
+}
+
+
+function ProductTable({products}:{products:Product[]}) {
+
+  const rows :JSX.Element[] = []
+  let lastCategory:null | string = null
+
+  products.forEach((product) => {
+
+  if(product.category !== lastCategory){
+    rows.push(
+      <ProductCategoryRow
+      category={product.category}
+      key={product.category}
+      price={product.price}
+      stocked ={product.stocked}
+      name = {product.name}
+      />
+    )
+  }
+  rows.push(<ProductRow
+            product={product}
+            key={product.name}/>
+  )
+      lastCategory = product.category;
+})
+
+  return(
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  )
+
+}
+
+function SearchBar(){
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label style={{ display: "block" }}>
+        <input type="checkBox" /> Only show products in stock
+      </label>
+    </form>
+  );
+}
+function FilterableProductTable({products}:{products:Product[]}){
+  return(
+    <div>
+      <SearchBar/>
+      <ProductTable products={products}/>
+    </div>
   )
 }
 
-export default App
+export default function App(){
+  return <FilterableProductTable products={products}/>
+}
